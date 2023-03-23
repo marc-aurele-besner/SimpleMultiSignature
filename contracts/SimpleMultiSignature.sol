a3067c2dafce7e41955b4ebd49d3115460b4c9ef
Guillaume-web <cryptomass2022@gmail.com>
2023-03-23 16:44:31 +0100

    #8

--- contracts/SimpleMultiSignature.sol
+++ contracts/SimpleMultiSignature.sol
@@ -33,7 +33,12 @@ contract SimpleMultiSignature {
 
   function changeOwner(address newOwner, address lastOwner) internal returns (bool) {}
 
-  function changeThreshold(uint256 newThreshold) internal returns (bool) {}
+    function changeThreshold(uint256 newThreshold) internal returns (bool) {
+      require(newThreshold > 0);
+      require(newThreshold <= _ownerCount);
+      _threshold = uint16(newThreshold);
+      return true;
+    }
 
   function reveive() external payable {}
 
