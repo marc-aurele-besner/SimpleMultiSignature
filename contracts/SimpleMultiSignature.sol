9baf96a0b61ac0fd1a0d50960cbbaa39ef7a3536
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-03-23 10:27:28 +0100

    #3

--- contracts/SimpleMultiSignature.sol
+++ contracts/SimpleMultiSignature.sol
@@ -4,4 +4,6 @@ pragma solidity ^0.8.19;
 contract SimpleMultiSignature {
   string public constant NAME = 'SimpleMultiSignature';
   string public constant VERSION = '0.0.1';
+
+  function isOwner(address userAddress) public returns (bool) {}
 }
